85e4e78c5284ce0997257c50af1d9cba88d1bfc1
Navid Rigi Ebrahimi <navid.rigi23@gmail.com>
2024-02-19 12:12:54 +0000

    finalized and seems working correctly

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,35 +3,41 @@
 #include <string.h>
 #include <strings.h>
 
+int isEndNewline(std::string original);
+
 int main(int argc, char **argv)
 {
+	size_t		i;
+	int			new_line = 0;
+	std::string	read;
+	size_t		index;
+
 	if (argc == 4)
 	{
 		std::string original = argv[1];
 		std::string replace = original + ".replace";
 		std::ifstream file1;
 		std::ofstream file2;
-		file1.open(original.c_str());
+		file1.open(original.c_str()); // open needs const char *
+		if (!file1.is_open())
+		{
+			std::cerr << "\033[1;31mError: File could not be opened\033[0m" << std::endl;
+			return 1;
+		}
 		file2.open(replace.c_str());
-
-		std::string read;
 		std::string s1 = argv[2];
 		std::string s2 = argv[3];
 		size_t s1_len = s1.size();
-		// size_t s2_len = s2.size();
-		size_t index;
-		size_t i = 0;
-		int new_line = 0;
-
 		while (std::getline(file1, read))
 		{
-			if (read.size())
-				new_line = 1;
-			while (read[i]) // until end of line
+			i = 0;
+			if (new_line)
+				file2 << std::endl;
+			while (read[i])
 			{
 				i = 0;
 				index = read.find(s1);
-				if (index == read.npos)
+				if (index == read.npos) // couldn't find
 					break;
 				else
 				{
@@ -41,15 +47,31 @@ int main(int argc, char **argv)
 				read = read.substr(index + s1_len);
 				file2 << s2;
 			}
-			while (i < read.size())
+			while (i < read.size()) // after last occurence till the end of line
 				file2 << read[i++];
-			if (new_line)
-				file2 << std::endl;
-			new_line = 0;
+			new_line = 1;
 		}
-		// while (std::getline(file2, read))
-		// 	std::cout << read << std::endl;
+		if (isEndNewline(original))
+			file2 << std::endl;
 		file1.close();
 		file2.close();
 	}
+	else
+		std::cout << "\033[1;31mExecute like this: ./sed fileName oldString newString\033[0m" << std::endl;
+	return 0;
+}
+
+int isEndNewline(std::string original)
+{
+	char			current;
+	char			last;
+	std::ifstream	file;
+
+	file.open(original.c_str());
+	while (file.get(current))
+		last = current;
+	file.close();
+	if (last == '\n')
+		return 1;
+	return 0;
 }
